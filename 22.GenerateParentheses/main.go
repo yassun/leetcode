56ab8e515df217b6ed5daa801085a70cac6ad1d8
yassun <yassun910@gmail.com>
2021-03-31 15:32:29 +0900

    22.GenerateParentheses

--- 22.GenerateParentheses/main.go
+++ 22.GenerateParentheses/main.go
@@ -1,5 +1,24 @@
 package main
 
+func rec(p string, left int, right int, max int, result *[]string) {
+	if left < right {
+		return
+	}
+	if left == max && right == max {
+		if len(p) > 0 {
+			*result = append(*result, p)
+		}
+	} else {
+		if left < max {
+			rec(p+"(", left+1, right, max, result)
+		}
+		if right < left {
+			rec(p+")", left, right+1, max, result)
+		}
+	}
+}
 func generateParenthesis(n int) []string {
-
+	result := make([]string, 0)
+	rec("", 0, 0, n, &result)
+	return result
 }
