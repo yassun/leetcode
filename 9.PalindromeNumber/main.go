c1773b2d3bf9bfe70adac7b3506d6e4ba599495f
yassun <yassun910@gmail.com>
2021-04-02 09:27:35 +0900

    9.PalindromeNumbee

--- 9.PalindromeNumber/main.go
+++ 9.PalindromeNumber/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+func isPalindrome(x int) bool {
+	if x < 0 {
+		return false
+	}
 
-func main() {
-	fmt.Println("vim-go")
+	var seq []int
+	seq = IntToSlice(x, seq)
+
+	left := 0
+	right := len(seq) - 1
+
+	for left < right {
+		if seq[left] == seq[right] {
+			left = left + 1
+			right = right - 1
+		} else {
+			break
+		}
+	}
+
+	if left < right {
+		return false
+	}
+
+	return true
+}
+
+func IntToSlice(n int, sequence []int) []int {
+	if n != 0 {
+		i := n % 10
+		sequence = append([]int{i}, sequence...)
+		return IntToSlice(n/10, sequence)
+	}
+	return sequence
 }
