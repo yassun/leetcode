5e03cc1815b4436671ac663225fc9d227ed58857
yassun <yassun910@gmail.com>
2021-04-03 16:28:30 +0900

    9.PalindromeNumbee

---
+++ 9.PalindromeNumber/revertHalfOfTheNumber.go
@@ -0,0 +1,16 @@
+package main
+
+func isPalindrome(x int) bool {
+
+	if x < 0 || (x%10 == 0 && x != 0) {
+		return false
+	}
+
+	revertedNumber := 0
+	for x > revertedNumber {
+		revertedNumber = revertedNumber*10 + x%10
+		x = x / 10
+	}
+
+	return x == revertedNumber || x == revertedNumber/10
+}
