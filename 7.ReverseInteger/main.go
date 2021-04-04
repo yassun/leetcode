369d25b09d14dcd7df1567094ba2a98a1b7b0c45
yassun <yassun910@gmail.com>
2021-04-04 18:52:01 +0900

    /7.ReverseInteger

--- 7.ReverseInteger/main.go
+++ 7.ReverseInteger/main.go
@@ -1,7 +1,24 @@
-package main
+/*
+const MaxInt32 = int(2147483647)
+const MinInt32 = int(-2147483648)
+*/
+import (
+	"math"
+)
 
-import "fmt"
+func reverse(x int) int {
+	result := 0
+	for x != 0 {
+		tmp := x % 10
+		x = x / 10
+		if result > math.MaxInt32/10 || (result == math.MaxInt32/10 && tmp > 7) {
+			return 0
+		}
+		if result < math.MinInt32/10 || (result == math.MinInt32/10 && tmp < -8) {
+			return 0
+		}
+		result = result*10 + tmp
+	}
 
-func main() {
-	fmt.Println("vim-go")
+	return result
 }
