0a94181b6d025a6bf0b9907edaf43afe397101da
yassun <yassun910@gmail.com>
2021-04-06 13:51:24 +0900

    13.RomanToIDnteger

--- 13.RomanToIDnteger/main.go
+++ 13.RomanToIDnteger/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+func romanToInt(s string) int {
+	romanValue := map[byte]int{
+		'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000,
+	}
 
-func main() {
-	fmt.Println("vim-go")
+	relust := 0
+	i := 0
+	for i < len(s) {
+		if i+1 < len(s) {
+			if romanValue[s[i]] < romanValue[s[i+1]] {
+				relust += romanValue[s[i+1]] - romanValue[s[i]]
+				i += 2
+				continue
+			} else {
+				relust += romanValue[s[i]]
+			}
+		} else {
+			relust += romanValue[s[i]]
+
+		}
+		i += 1
+	}
+	return relust
 }
